8661e0b6cd14676eb5d70448701624e69aaa238d
Gcody-Booth <gcody@chicagobooth.edu>
2022-12-06 06:02:03 +0000

    Airport code passes via form and params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def home
+
+    render({ :template => "main_page/home.html.erb" })
+  end
+
+  def wait_time
+    @airport_code = params.fetch(:airport_code)
+    render({ :template => "main_page/wait_time.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "home" })
+  get("/wait_time", { :controller => "application", :action => "wait_time" })
 
 
 end
