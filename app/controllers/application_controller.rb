4e265c4dad13b8b415e445c83cfb3b995964ceb2
Gcody-Booth <gcody@chicagobooth.edu>
2022-12-06 07:09:39 +0000

    Wait time works Wait time now working. Airport names now formatted. Still need to get drive time API working successfully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,22 +29,32 @@ class ApplicationController < ActionController::Base
     @origin_longitude = loc.fetch("lng")  
 
 
-    #generate lat/lon for airport using TSAWaittime API call
+    #generate lat/lon and name for airport using TSAWaittime API call
     tsa_wait_api_endpoint = "https://www.tsawaittimes.com/api/airport/IpFPh8X6u9f9t1B8wgGLMTLKO6tqQ1Pc/" + @airport_code
     raw_wait_time_data = URI.open(tsa_wait_api_endpoint).read
     parsed_wait_data = JSON.parse(raw_wait_time_data)
     @destination_lat = parsed_wait_data.fetch("latitude")
     @destination_lon = parsed_wait_data.fetch("longitude")
+    @formatted_name = parsed_wait_data.fetch("name")
+
+    #find security line wait time
+    @security_wait_time = parsed_wait_data.fetch("rightnow")
 
 
-    #Calculate Drive time
+
+  #   Calculate Drive time
   #   https://maps.googleapis.com/maps/api/distancematrix/json
   # ?destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626
   # &origins=40.6655101%2C-73.89188969999998
   # &key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U
 
+  # https://maps.googleapis.com/maps/api/distancematrix/json
+  # ?destinations= @destination_lat + "%2C" + @destination_lon
+  # &origins=@origin_latitude+ "%2C" + @origin_longitude
+  # &key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U
 
-    #find security line wait time
+
+
 
     render({ :template => "main_page/wait_time.html.erb" })
   end
