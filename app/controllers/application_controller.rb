b71e4f3a4a702eebf950a8e9f144dcb2f6b3eac6
Gcody-Booth <gcody@chicagobooth.edu>
2022-12-06 06:46:33 +0000

    lat and lon pulls working for both origin and destination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,13 @@ class ApplicationController < ActionController::Base
   end
 
   def wait_time
+    #Initial user inputs
     @airport_code = params.fetch(:airport_code)
     @pre_google_location = params.fetch(:location)
 
+
+
+    #format user address and get lat/lon
     gmaps_api_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @pre_google_location + "&key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U"
     require("open-uri")
     raw_data = URI.open(gmaps_api_endpoint).read
@@ -19,6 +23,28 @@ class ApplicationController < ActionController::Base
     first_results = results_array.at(0)
     @cleaned_address = first_results.fetch("formatted_address")
 
+    geo = first_results.fetch("geometry")
+    loc = geo.fetch("location")
+    @origin_latitude =  loc.fetch("lat")
+    @origin_longitude = loc.fetch("lng")  
+
+
+    #generate lat/lon for airport using TSAWaittime API call
+    tsa_wait_api_endpoint = "https://www.tsawaittimes.com/api/airport/IpFPh8X6u9f9t1B8wgGLMTLKO6tqQ1Pc/" + @airport_code
+    raw_wait_time_data = URI.open(tsa_wait_api_endpoint).read
+    parsed_wait_data = JSON.parse(raw_wait_time_data)
+    @destination_lat = parsed_wait_data.fetch("latitude")
+    @destination_lon = parsed_wait_data.fetch("longitude")
+
+
+    #Calculate Drive time
+  #   https://maps.googleapis.com/maps/api/distancematrix/json
+  # ?destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626
+  # &origins=40.6655101%2C-73.89188969999998
+  # &key=AIzaSyAgRzRHJZf-uoevSnYDTf08or8QFS_fb3U
+
+
+    #find security line wait time
 
     render({ :template => "main_page/wait_time.html.erb" })
   end
